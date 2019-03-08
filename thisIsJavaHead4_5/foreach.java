c50d3760006f9d5852e4aae66492e8fdc295b998
user21_11 <user21_11@mail.ru>
2019-03-08 19:05:39 +0600

    Release

--- thisIsJavaHead4_5/foreach.java
+++ thisIsJavaHead4_5/foreach.java
@@ -1,10 +1,22 @@
 package thisIsJavaHead4_5;
 
+
+import java.util.Random;
+
 public class foreach {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int N = 10;
+		Random rand = new Random(50);
+		float f[] = new float[N];
+		for (int i =0; i<N; i++)
+			f[i] =	rand.nextFloat();
+		for(float x : f)
+			System.out.println(x);
+		
+		for(char c:"My woonder pike is under light strike".toCharArray())
+			System.out.print(c+" ");
+		
 	}
 
 }
